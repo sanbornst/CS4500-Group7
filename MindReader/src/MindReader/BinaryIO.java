775d635036f2794454114e7060c889cf5cd2698f
Chris <curreri.c@husky.neu.edu>
2015-02-25 11:33:47 -0500

    BinaryIO stub

---
+++ MindReader/src/MindReader/BinaryIO.java
@@ -0,0 +1,44 @@
+package MindReader;
+
+import info.monitorenter.gui.chart.ITrace2D;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * Binary File IO Object
+ * 
+ * <p>Handles reading from mindware .mw files, parsing them into individual
+ * channels that can be read from.
+ * 
+ * @author Christopher Curreri
+ */
+public class BinaryIO implements FileIO {
+    //TODO Migrate prototype code
+    
+    @Override
+    public void open(String path) throws IOException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void close() throws IOException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void read(ITrace2D channel, int id, int start, int length,
+            int frequency) throws IOException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public HashMap<Integer, String> getChannels() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
