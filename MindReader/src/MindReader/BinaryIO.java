81f286caae3b02cc8f2c8b971e4cd82379e07901
Chris <curreri.c@husky.neu.edu>
2015-03-04 14:22:10 -0500

    push channel information reading to its own function

--- MindReader/src/MindReader/BinaryIO.java
+++ MindReader/src/MindReader/BinaryIO.java
@@ -18,9 +18,9 @@ import java.nio.ByteBuffer;
  * @author Christopher Curreri
  */
 public class BinaryIO implements FileIO {
-    private FileInputStream source;          // Input file stream
-    private int headerLength;                // Length of the header
-    private ArrayList<ChannelInfo> channels; // ArrayList for channel info
+    private FileInputStream source;          // input file stream
+    private int headerLength;                // length of the header
+    private ArrayList<ChannelInfo> channels; // arrayList for channel info
     
     BinaryIO(){
         this.source = null;
@@ -29,88 +29,96 @@ public class BinaryIO implements FileIO {
     }
     
     private void readHeader() throws IOException{
-        byte[] temp = new byte[8];
+        byte[] firstBytes = new byte[8];
         ByteBuffer bb;
         
         System.out.println("Reading Header...");
         
-        // Read the first eight bytes from file and store in a ByteBuffer
-        this.source.read(temp, 0, 8);
-        bb = ByteBuffer.wrap(temp);
+        // read the first eight bytes from file and store in a ByteBuffer
+        this.source.read(firstBytes, 0, 8);
+        bb = ByteBuffer.wrap(firstBytes);
         
-        // Pull two ints out of the byte buffer (8 bytes)
+        // pull two ints out of the byte buffer (8 bytes)
         int byte1 = bb.getInt();
         int byte2 = bb.getInt();
         
-        // Verify file is a MindWare file (byte2 = byte1 - 4)
+        // verify file is a MindWare file (byte2 = byte1 - 4)
         if (byte2 == byte1 - 4){
-            // Valid file, store header length and continue
+            // valid file, store header length and continue
             this.headerLength = byte1;
-            System.out.println("Header Length: " + this.headerLength);
+            System.out.println("\tHeader Length: " + this.headerLength);
         } else {
-            // Invalid file, throw exception
+            // invalid file, throw exception
             throw new IOException("Not a MindWare File!");
         }
         
-        // Get the header information from stream (less the first eight bytes)
+        // get the header information from stream (less the first eight bytes)
         byte[] header = new byte[this.headerLength - 8];
         this.source.read(header, 0, this.headerLength - 8);
         bb = ByteBuffer.wrap(header);
         
-        // Length of raw channel list
+        // length of raw channel list
         int channelNameLength = bb.getInt();
         
-        // Get and ignore the raw channel list (information is located later)
-        byte[] channelNames = new byte[channelNameLength];
-        bb.get(channelNames, 0, channelNameLength);
+        // ignore the raw channel list (information is located later)
+        bb.position(bb.position() + channelNameLength);
         
-        // Get and ignore the hardware configuration information as it is unimportant
+        // ignore the hardware configuration information as it is unimportant
         // TODO: See if there's anything useful in here
-        bb.get(bb.getInt());
+        bb.position(bb.position()+  4);
         
-        // Get number of channels
+        // get number of channels
         int numChannels = bb.getInt();
         
-        System.out.println("Number of channels: " + numChannels);
+        System.out.println("\tNumber of channels: " + numChannels);
+
+        int channelLength = 0;
         
-        
-        // TODO: Figure out how to split this out into it's own function
-        //       duplicate buffer, and remember how much was read then
-        //       update original buffer by skipping that many bytes at the end
+        // get all the channel information
+        for (int i = 0; i < numChannels; i++){
+            // get the channel information
+            channelLength = readChannel(bb.duplicate());
+            
+            // ignore channelLength bytes from main buffer
+            bb.position(bb.position() + channelLength);
+        }
+    }
+    
+    private int readChannel(ByteBuffer bb){
         String name = null;
         byte[] bName = null;
         int nameLength = -1;
         
-        // Get all the channel information
-        for (int i = 0; i < numChannels; i++){
-            // Get the channel name length
-            nameLength = bb.getInt();
-            bName = new byte[nameLength];
-            
-            // Get the bytes that make up the channel name
-            bb.get(bName, 0, nameLength);
-            
-            // Attempt to convert them into an ASCII string
-            try{
-                name = new String(bName, "US-ASCII").trim();
-            } catch (Exception e) {
-                // TODO: Meaningful error here
-            }
-            
-            // Pull the rest of the channel data
-            // TODO: Put useful channel data into ChannelInfo class
-            bb.getFloat();
-            bb.getFloat();
-            bb.getFloat();
-            bb.getShort();
-            bb.getFloat();
-            bb.getShort();
-            bb.getShort();
-            bb.getFloat();
-            bb.getFloat();
-            
-            System.out.println("Channel Name: \"" + name + "\"");
+        // Get the channel name length
+        nameLength = bb.getInt();
+        bName = new byte[nameLength];
+        
+        // Get the bytes that make up the channel name
+        bb.get(bName, 0, nameLength);
+        
+        // Attempt to convert them into an ASCII string
+        try{
+            name = new String(bName, "US-ASCII").trim();
+        } catch (Exception e) {
+            // TODO: Meaningful error here
         }
+        
+        // Pull the rest of the channel data
+        // TODO: Put useful channel data into ChannelInfo class
+        bb.getFloat();
+        bb.getFloat();
+        bb.getFloat();
+        bb.getShort();
+        bb.getFloat();
+        bb.getShort();
+        bb.getShort();
+        bb.getFloat();
+        bb.getFloat();
+        
+        System.out.println("\t\tChannel Name: \"" + name + "\"");
+        
+        // length bytes + name bytes + rest bytes
+        return 4 + nameLength + 30;
     }
     
     public void open(String path) throws IOException {
