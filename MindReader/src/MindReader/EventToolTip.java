4029e1cd4f1d910a3071e32e4a0775f62d88080b
Samantha Sanborn <sanborn.s@husky.neu.edu>
2015-03-31 23:26:29 -0400

    EventToolTip update
    EventToolTip differentiates between point types, so the event trace can
    go on any chart without messing things up

--- MindReader/src/MindReader/EventToolTip.java
+++ MindReader/src/MindReader/EventToolTip.java
@@ -2,6 +2,7 @@ package MindReader;
 
 import info.monitorenter.gui.chart.Chart2D;
 import info.monitorenter.gui.chart.IToolTipType;
+import info.monitorenter.gui.chart.ITracePoint2D;
 import java.awt.event.MouseEvent;
 
 class EventToolTip implements IToolTipType {
@@ -17,7 +18,12 @@ class EventToolTip implements IToolTipType {
   
   @Override
   public String getToolTipText(Chart2D cd, MouseEvent me) {
-    TracePointString p = (TracePointString) cd.getNearestPointManhattan(me);
-    return p.getText();
+    ITracePoint2D p = cd.getNearestPointManhattan(me);
+    if (p instanceof TracePointString) {
+      TracePointString extraDataPoint = (TracePointString) p;
+      return extraDataPoint.getText();
+    } else {
+      return "";
+    }
   }
 }
