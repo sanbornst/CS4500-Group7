ff897799a32df21224c84bd5cd7e385987bb89a7
Samantha Sanborn <sanborn.s@husky.neu.edu>
2015-04-13 13:33:32 -0400

    added error bars to events, made file filters
    also added documentation comments

--- MindReader/src/MindReader/EventChartTest.java
+++ MindReader/src/MindReader/EventChartTest.java
@@ -5,30 +5,41 @@ import info.monitorenter.gui.chart.Chart2D;
 import info.monitorenter.gui.chart.ITrace2D;
 import info.monitorenter.gui.chart.traces.Trace2DSimple;
 import info.monitorenter.gui.chart.traces.painters.TracePainterDisc;
+import info.monitorenter.gui.chart.errorbars.*;
+import info.monitorenter.gui.chart.IErrorBarPolicy;
+import info.monitorenter.gui.chart.IErrorBarPainter;
+import info.monitorenter.gui.chart.pointpainters.PointPainterLine;
+import info.monitorenter.gui.chart.pointpainters.PointPainterDisc;
 import java.io.IOException;
+import java.awt.Color;
 
 class EventChartTest{
   public static void main(String[] args) throws IOException {
     JFrame testFrame = new JFrame("Testing Events");
     Chart2D testChart = new Chart2D();
     ITrace2D trace0 = new Trace2DSimple();
-    ITrace2D trace1 = new Trace2DSimple();
-    trace0.setTracePainter(new TracePainterDisc());
-    trace0.setColor(java.awt.Color.RED);
+    //trace0.setTracePainter(new TracePainterDisc());
+    //trace0.setColor(java.awt.Color.RED);
     testChart.setToolTipType(new EventToolTip());
     testChart.addTrace(trace0);
-    testChart.addTrace(trace1);
+    //IErrorBarPolicy errors = new ErrorBarPolicyAbsoluteSummation(5, 5);
+    //errors.setShowNegativeYErrors(true);
+    //errors.setShowPositiveYErrors(true);
+    //IErrorBarPainter errorPainter = new ErrorBarPainterLine();
+    //errors.setErrorBarPainter(errorPainter);
+    //errorPainter.setEndPointPainter(new PointPainterDisc());
+    //errorPainter.setEndPointColor(Color.WHITE);
+    //errorPainter.setConnectionPainter(new PointPainterLine());
+    //errorPainter.setConnectionColor(Color.RED);
+    //trace0.setErrorBarPolicy(errors);
     
     EventIO testEvent = new EventIO();
-    testEvent.open("PA_1_event.txt");
-    testEvent.read(trace0, 0, 0, 300000, 0);
-    
-    BinaryIO testBin = new BinaryIO();
-    testBin.open("PA_1.mw");
-    testBin.read(trace1, 0, 0, 300000, 200);
+    testEvent.open("CFSArmy1B_Pp37_7-14-14_event.txt");
+    testEvent.read(trace0, 0, 700000, 900000, 0);
     
     
     testFrame.add(testChart);
+    testFrame.pack();
     testFrame.setVisible(true);
   }
 }
--- MindReader/src/MindReader/EventToolTip.java
+++ MindReader/src/MindReader/EventToolTip.java
@@ -5,17 +5,33 @@ import info.monitorenter.gui.chart.IToolTipType;
 import info.monitorenter.gui.chart.ITracePoint2D;
 import java.awt.event.MouseEvent;
 
+/**
+ * Implements JChart2D's IToolTipType interface to display event
+ * information from its custom trace point
+ * 
+ * @author Samantha Sanborn
+ */
 class EventToolTip implements IToolTipType {
   
+  // empty constructor
   EventToolTip() {
     
   }
   
+  /**
+   * Returns a description of the behavior of the tooltip type
+   */
   @Override
   public String getDescription() {
     return "Values, snap to nearest point";
   }
   
+  /**
+   * Returns the appropriate tooltip text for any point where the mouse is on the chart
+   * 
+   * @param cd the chart being moused over
+   * @param me mouse event to determine location of mouse
+   */
   @Override
   public String getToolTipText(Chart2D cd, MouseEvent me) {
     ITracePoint2D p = cd.getNearestPointManhattan(me);
